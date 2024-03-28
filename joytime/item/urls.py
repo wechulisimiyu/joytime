5c685aa4954999d11982e11a5775f829b00a85f4
wechu07 <wechu017@gmail.com>
2024-03-28 15:23:35 +0300

    add items

--- joytime/item/urls.py
+++ joytime/item/urls.py
@@ -6,7 +6,7 @@ app_name = 'item'
 
 urlpatterns = [
     # path('', views.items, name='items'),
-    # path('new/', views.new, name='new'),
+    path('new/', views.new, name='new'),
     path('<int:pk>/', views.detail, name='detail'),
     # path('<int:pk>/delete/', views.delete, name='delete'),
     # path('<int:pk>/edit/', views.edit, name='edit'),
