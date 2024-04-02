018df888013bdabb13002446c4ba87e0aef5dcb8
wechulisimiyu <wechu017@gmail.com>
2024-04-02 20:45:10 +0300

    fix db for dev and prod mode

--- joytime/settings.py
+++ joytime/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import environ 
+import os
 from django.core.management.utils import get_random_secret_key
 
 env = environ.Env(  # <-- Updated!
@@ -102,14 +103,25 @@ WSGI_APPLICATION = 'joytime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
-    'default': env.db(),
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+#     # 'default': env.db(),
+# }
 
+if os.getenv('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': env.db(),
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
