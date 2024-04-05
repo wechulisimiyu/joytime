42a03d2469c99338834a2b876d2772dd3729ce03
wechulisimiyu <wechu017@gmail.com>
2024-04-05 16:03:10 +0300

    update db

--- joytime/settings.py
+++ joytime/settings.py
@@ -103,25 +103,25 @@ WSGI_APPLICATION = 'joytime.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     },
-#     # 'default': env.db(),
-# }
+DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
+    'default': env.db(),
+}
 
-if os.getenv('DJANGO_ENV') == 'production':
-    DATABASES = {
-        'default': env.db(),
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# if os.getenv('DJANGO_ENV') == 'production':
+#     DATABASES = {
+#         'default': env.db(),
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
